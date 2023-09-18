ff5f9fe367a1906f2dca19afaf29320e2d0ac35b
ArshadQ118 <arshadq118@gmail.com>
2023-09-18 18:58:32 +0530

    add new mode

--- audioapp/utils.py
+++ audioapp/utils.py
@@ -2,7 +2,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from transformers import pipeline
+import speech_recognition as sr
 from dotenv import load_dotenv
 from .prompts import *
 import random
@@ -10,7 +10,19 @@ import openai
 import os
 
 load_dotenv()
-cls = pipeline('automatic-speech-recognition')
+
+
+def getText(audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio_data = recognizer.record(source)
+    try:
+        transcribed_text = recognizer.recognize_google(audio_data)
+        return transcribed_text
+    except sr.UnknownValueError:
+        return ""
+    except sr.RequestError as e:
+        return ""
 
 
 def getHuggingFace(text):
@@ -43,10 +55,10 @@ def getGPTSummary(input_text):
 
 def get_Text_from_file(filepath):
     try:
-        res = cls(filepath)
+        res = getText(filepath)
         random_value = random.random()
-        summarized = getHuggingFace(res['text']) if random_value < 0.5 else getGPTSummary(res['text'])
-        return summarized, res['text']
+        summarized = getHuggingFace(res) if random_value < 0.5 else getGPTSummary(res)
+        return summarized, res
     except Exception as e:
         return "I'm very sorry, but I can't assist with that", ""
 
